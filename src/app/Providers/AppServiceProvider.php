ebdfeda0e7f6c670149231b2ecb44bcc2f8de8d8
坚硬的石头 <do_nb@outlook.com>
2018-01-07 21:19:09 +0800

    [f01002] 生成默认用户管理

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 限制最大字符数
+        Schema::defaultStringLength(191);
     }
 
     /**
