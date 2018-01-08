0885d13a97cd1489b0a5afc894f09f401784e15f
坚硬的石头 <do_nb@outlook.com>
2018-01-08 14:17:36 +0800

    [f01004] home 视图：增加按钮

--- src/resources/views/partials/articles.blade.php
+++ src/resources/views/partials/articles.blade.php
@@ -1,9 +1,32 @@
+<div>
+@auth()
+    <a href="{{ route('home') }}" class="btn btn-primary">
+    	<span class="glyphicon glyphicon-plus-sign"></span>
+    	&nbsp;增加
+    </a>
+@endauth
+</div>
+<div>&nbsp;</div>
+<div>
 <ul>
 @foreach ($articles as $article)
     <li>
         <a href=""> {{ $article->title }} </a>
         ({{ $article->updated_at }})
+		@can('update', $article)
+		    <a href="{{ route('home') }}" class="btn btn-primary">
+		    	<span class="glyphicon glyphicon-edit"></span>
+		    	&nbsp;编辑
+		    </a>
+		@endcan
+		@can('delete', $article)
+		    <a href="{{ route('home') }}" class="btn btn-primary">
+		    	<span class="glyphicon glyphicon-remove-sign"></span>
+		    	&nbsp;删除
+		    </a>
+		@endcan
     </li>
 @endforeach
 </ul>
 {{ $articles->links() }}
+</div>
