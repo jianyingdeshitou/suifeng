dd420d8653999f65c22e98bc92b28440388c9957
坚硬的石头 <do_nb@outlook.com>
2018-01-08 13:29:46 +0800

    [f01004] 账户菜单添加home链接

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -48,15 +48,24 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    <span class="glyphicon glyphicon-user"> </span>
+                                    &nbsp;{{ Auth::user()->name }}
+                                    <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
                                     <li>
+                                        <a href="{{ route('home') }}">
+                                            <span class="glyphicon glyphicon-home"></span>
+                                            &nbsp;Home
+                                        </a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            <span class="glyphicon glyphicon-log-out"></span>
+                                            &nbsp;Logout
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
