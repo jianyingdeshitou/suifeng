27c86d26faee454b1a15a21fb452419f725ea2fa
坚硬的石头 <do_nb@outlook.com>
2018-01-15 09:44:44 +0800

    [f01006] 表 articles 添加 published_at 字段

--- src/database/factories/ArticleFactory.php
+++ src/database/factories/ArticleFactory.php
@@ -2,11 +2,14 @@
 
 use Faker\Generator as Faker;
 
+use App\User;
+
 $factory->define(App\Article::class, function (Faker $faker) {
 	$user_ids = User::pluck('id')->toArray();
     return [
-        'title' => $faker->sentence(),
-        'content' => $faker->text(),
+        'title' => $faker->sentence(mt_rand(3, 10)),
+        'content' => join("\n\n", $faker->paragraphs(mt_rand(3, 6))),
         'user_id' => $faker->randomElement($user_ids),
+        'published_at' => $faker->dateTimeBetween('-1 month', '+3 days'),
     ];
 });
